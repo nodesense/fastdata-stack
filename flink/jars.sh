6655dd31d7904b230c71e730d5b8ba8ede5c20bf
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2022-08-03 17:54:22 +0530

    docker compose

--- flink/jars.sh
+++ flink/jars.sh
@@ -1,3 +1,8 @@
 wget -P mount/jars https://repo1.maven.org/maven2/org/apache/flink/flink-connector-jdbc/1.15.0/flink-connector-jdbc-1.15.0.jar
 wget -P mount/jars https://repo1.maven.org/maven2/mysql/mysql-connector-java/8.0.29/mysql-connector-java-8.0.29.jar
 wget  -P mount/jars https://repo1.maven.org/maven2/org/apache/flink/flink-shaded-hadoop2-uber/2.7.5-1.8.3/flink-shaded-hadoop2-uber-2.7.5-1.8.3.jar
+
+wget  -P mount/jars  https://repo1.maven.org/maven2/org/apache/flink/flink-table-api-java-bridge/1.15.1/flink-table-api-java-bridge-1.15.1.jar
+
+wget  -P mount/jars https://repo1.maven.org/maven2/org/apache/flink/flink-connector-hive_2.12/1.15.1/flink-connector-hive_2.12-1.15.1.jar
+wget  -P mount/jars https://repo1.maven.org/maven2/org/apache/hive/hive-exec/2.3.6/hive-exec-2.3.6.jar
