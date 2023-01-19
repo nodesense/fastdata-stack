2f9767180913dac881550e493fbe1fad80242bd7
Gopalakrisnan Subramani <gopalakrishnan.subramani@gmail.com>
2023-01-19 20:05:15 +0530

    flink 1.15.3 and patch docker compose with 3 instance of task

--- flink/jars.sh
+++ flink/jars.sh
@@ -1,18 +1,18 @@
-wget -P mount/jars https://repo1.maven.org/maven2/org/apache/flink/flink-connector-jdbc/1.15.0/flink-connector-jdbc-1.15.0.jar
+wget -P mount/jars https://repo1.maven.org/maven2/org/apache/flink/flink-connector-jdbc/1.15.3/flink-connector-jdbc-1.15.3.jar
 wget -P mount/jars https://repo1.maven.org/maven2/mysql/mysql-connector-java/8.0.29/mysql-connector-java-8.0.29.jar
 wget  -P mount/jars https://repo1.maven.org/maven2/org/apache/flink/flink-shaded-hadoop2-uber/2.7.5-1.8.3/flink-shaded-hadoop2-uber-2.7.5-1.8.3.jar
 
-wget  -P mount/jars  https://repo1.maven.org/maven2/org/apache/flink/flink-table-api-java-bridge/1.15.1/flink-table-api-java-bridge-1.15.1.jar
+wget  -P mount/jars  https://repo1.maven.org/maven2/org/apache/flink/flink-table-api-java-bridge/1.15.3/flink-table-api-java-bridge-1.15.3.jar
 
-wget  -P mount/jars https://repo1.maven.org/maven2/org/apache/flink/flink-connector-hive_2.12/1.15.1/flink-connector-hive_2.12-1.15.1.jar
+wget  -P mount/jars https://repo1.maven.org/maven2/org/apache/flink/flink-connector-hive_2.12/1.15.3/flink-connector-hive_2.12-1.15.3.jar
 wget  -P mount/jars https://repo1.maven.org/maven2/org/apache/hive/hive-exec/2.3.6/hive-exec-2.3.6.jar
 
-wget  -P mount/jars https://repo1.maven.org/maven2/org/apache/flink/flink-sql-connector-kafka/1.15.1/flink-sql-connector-kafka-1.15.1.jar
+wget  -P mount/jars https://repo1.maven.org/maven2/org/apache/flink/flink-sql-connector-kafka/1.15.3/flink-sql-connector-kafka-1.15.3.jar
 
-wget  -P mount/jars https://repo1.maven.org/maven2/org/apache/flink/flink-parquet/1.15.1/flink-parquet-1.15.1.jar
+wget  -P mount/jars https://repo1.maven.org/maven2/org/apache/flink/flink-parquet/1.15.3/flink-parquet-1.15.3.jar
 
 
-wget  -P mount/jars https://repo1.maven.org/maven2/org/apache/flink/flink-orc/1.15.1/flink-orc-1.15.1.jar
+wget  -P mount/jars https://repo1.maven.org/maven2/org/apache/flink/flink-orc/1.15.3/flink-orc-1.15.3.jar
 
 
 
